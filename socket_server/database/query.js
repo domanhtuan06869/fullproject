6d334f35eba02ce5b90054c520badcd79995e144
tuan.domanh <tuan.domanh>
2020-10-08 00:41:20 +0700

    add close mysql

--- socket_server/database/query.js
+++ socket_server/database/query.js
@@ -9,4 +9,7 @@ module.exports = async (conn, query) => new Promise(
         resolve(result);
       };
       conn.query(query, handler);
+      conn.end(()=>{
+        console.log('close connect mysql');
+      });
     });
